c3e0f1228f3421b6a7ce641e64fec446ad8c29b4
chensai <cs1324332517@163.com>
2022-12-13 10:30:06 +0800

    feat: 添加request示例

---
+++ error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="error">
+    404 page show
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- services/http.js
+++ services/http.js
@@ -1,1 +1,14 @@
 // http request function [$fetch]
+
+// 发送请求 post 示例
+// const config = useRuntimeConfig();
+// const res = await $fetch(`${config.VITE_API_MENU_URL}/menu-api/saveHomePagePreRegistrationInfo`, {
+//   method: 'POST',
+//   body: {
+//     preRegistrationInfo: {
+//       ...formObj,
+//       informationFrom: 'HomePage',
+//       browserIdentity: localStorage.getItem(BROWSER_IDENTIFICATION_KEY),
+//     },
+//   },
+// });
