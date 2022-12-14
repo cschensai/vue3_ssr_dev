2803a702b0b457b1e2a77a669dd9b19713255dd7
YufengLeng <YufengLeng@desty.app>
2022-12-14 18:33:25 +0800

    feat:Experience 和Toolss

--- components/Experience/index.vue
+++ components/Experience/index.vue
@@ -1,12 +1,127 @@
 <template>
-  <section class="experience"></section>
+  <section class="experience">
+    <p class="experience-title">Experience Desty free for 14 days</p>
+    <p class="experience-des">
+      Start managing your business in Desty ecosystem now!
+    </p>
+    <div class="experience-btn" @click="goExperience">
+      <span>Start Free Trial</span>
+      <img
+        src="https://static.desty.app/desty-homepage/v2/right_arrow.svg"
+        alt=""
+      />
+    </div>
+    <img
+      class="white-supers"
+      src="https://static.desty.app/desty-homepage/v2/white-supers.png"
+      alt=""
+    />
+    <img
+      class="yellow-supers"
+      src="https://static.desty.app/desty-homepage/v2/yellow-supers.png"
+      alt=""
+    />
+  </section>
 </template>
-<script setup></script>
+<script setup>
+const goExperience = () => {
+  window.location.href = "https://omni.desty.app/register";
+};
+</script>
 <style lang="less" scoped>
 .experience {
   width: 100%;
   height: 541px;
+  display: flex;
+  flex-direction: column;
+  // justify-content: center;
+  align-items: center;
   background: #eff2ff;
   border-radius: 20px;
+  font-family: "Plus Jakarta Sans";
+  font-style: normal;
+  position: relative;
+  &-title {
+    font-weight: 700;
+    font-size: 56px;
+    line-height: 71px;
+    text-align: center;
+    color: #000000;
+    z-index: 1;
+  }
+  &-des {
+    width: 374px;
+    height: 80px;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 40px;
+    text-align: center;
+    color: #000000;
+  }
+  &-btn {
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    // padding: 16px 20px;
+    gap: 12px;
+    width: 193px;
+    height: 53px;
+    background: #0c1bb8;
+    border: 2px solid #0c1bb8;
+    border-radius: 12px;
+    cursor: pointer;
+    span {
+      font-family: "Public Sans";
+      font-style: normal;
+      font-weight: 600;
+      font-size: 18px;
+      line-height: 21px;
+      letter-spacing: -0.01em;
+      color: #ffffff;
+    }
+  }
+  .white-supers {
+    position: absolute;
+    left: -38px;
+    top: -60px;
+  }
+  .yellow-supers {
+    position: absolute;
+    right: 0;
+    top: 214px;
+  }
+}
+@media screen and (max-width: 768px) {
+  .experience {
+    height: 324px;
+    &-title {
+      width: 329px;
+      height: 84px;
+      font-weight: 700;
+      font-size: 30px;
+      line-height: 42px;
+      margin-top: 40px;
+    }
+    &-des {
+      width: 274px;
+      height: 60px;
+      font-weight: 400;
+      font-size: 18px;
+      line-height: 30px;
+      margin-top: 16px;
+      margin-bottom: 12px;
+      z-index: 1;
+    }
+    .white-supers {
+      left: -160px;
+      top: -100px;
+    }
+    .yellow-supers {
+      right: -231px;
+      top: 176px;
+    }
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <!-- <ElButton type="danger" @click="handleCount">add count, {{ currentCount }}</ElButton> -->
     <!-- <ElButton type="danger" @click="handleNumber">add number, {{ currentNumber }}</ElButton> -->
     <!-- <nuxt-img preload src="https://static.desty.app/desty-store/philippines-img.png" loading="lazy" width="200px" height="200px" /> -->
+    <tools />
     <relation />
     <experience />
   </div>
@@ -10,24 +11,27 @@
 
 <script setup>
 // import { ElButton } from 'element-plus';
-import useUser from '~~/stores';
-import Relation from '~~/components/Relation';
-import experience from '~~/components/Experience'
+import useUser from "~~/stores";
+import Relation from "~~/components/Relation";
+import experience from "~~/components/Experience";
+import tools from "~~/components/Tools";
 
 useHead({
-  title: 'index page',
-  meta: [{
-    name: 'description',
-    content: 'description content',
-  }]
-})
+  title: "index page",
+  meta: [
+    {
+      name: "description",
+      content: "description content",
+    },
+  ],
+});
 
 const currentCount = useCount();
 
 const store = useUser();
 const currentNumber = computed(() => {
   return store.number;
-})
+});
 // composable
 function handleCount() {
   currentCount.value++;
@@ -36,9 +40,6 @@ function handleCount() {
 function handleNumber() {
   store.addNumber();
 }
-
-
-
 </script>
 
 <style lang="less" scoped>
